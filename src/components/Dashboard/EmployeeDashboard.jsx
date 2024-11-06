40c931a5c90583f91c3618f9262c0ed851b5f297
Sheikhzain07 <sheikhzain459@gmail.com>
2024-11-06 16:10:19 +0530

    created header of employee dashboard

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../others/Header";
+
+const EmployeeDashboard = () => {
+  return (
+    <div className="p-10 bg-[#1c1c1f] h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default EmployeeDashboard;

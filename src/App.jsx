426250ea8c608e85d277447be1554182a87035d6
Sheikhzain07 <sheikhzain459@gmail.com>
2024-11-07 23:55:38 +0530

    checked the employee credential to match with the localstorage data and then employee can login to the ems and written a logic for role i.e. admin or employee and set them to the localstorage with the role name

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,33 @@ import { AuthContext } from "./context/AuthProvider";
 function App() {
   const [user, setUser] = useState(null);
 
+  const authData = useContext(AuthContext);
+
+  const loggedInUser = localStorage.getItem("loggedInUser");
+  // console.log(loggedInUser);
+
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
       setUser("admin");
-      console.log("admin");
-    } else if (email == "emp@me.com" && password == "123") {
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+    } else if (
+      authData &&
+      authData.employees.find((e) => email == e.email && password == e.password)
+    ) {
       setUser("employee");
-      console.log("employee");
+      localStorage.setItem(
+        "loggedInUser",
+        JSON.stringify({ role: "employee" })
+      );
     } else {
       alert("invalid");
     }
   };
 
-  const data = useContext(AuthContext);
-  console.log(data);
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ""}
-      {user == "employee" ? <EmployeeDashboard /> : <AdminDashboard />}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   );
 }
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,11 +1,20 @@
-import React, { createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
+import { getLocalStorage } from "../utils/localStorage";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const { employees, admin } = getLocalStorage();
+    setUserData({ employees, admin });
+    // console.log(employees, admin);
+  }, []);
+
   return (
     <div>
-      <AuthContext.Provider value={"zain"}>{children}</AuthContext.Provider>
+      <AuthContext.Provider value={userData}>{children}</AuthContext.Provider>
     </div>
   );
 };

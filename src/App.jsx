460892d9181659e8c291a3d2b316710a7191fcbd
Sheikhzain07 <sheikhzain459@gmail.com>
2024-11-07 15:52:09 +0530

    written a logic for test employee and admin page depending on the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Login from "./components/Auth/Login";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 
 function App() {
-  useEffect(() => {
-    // setLocalStorage();
-    getLocalStorage();
-  });
+  const [user, setUser] = useState(null);
 
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      setUser("admin");
+      console.log("admin");
+    } else if (email == "emp@me.com" && password == "123") {
+      setUser("employee");
+      console.log("employee");
+    } else {
+      alert("invalid");
+    }
+  };
   return (
     <>
-      {/* <Login /> */}
-      {/* <EmployeeDashboard /> */}
-      <AdminDashboard />
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   );
 }

946721290ce6b1472ddfc768ebcf84d039502224
Sheikhzain07 <sheikhzain459@gmail.com>
2024-11-08 23:46:38 +0530

    written logic for the number of task from the auth for all the employee

--- src/components/others/TaskListNumbers.jsx
+++ src/components/others/TaskListNumbers.jsx
@@ -4,20 +4,20 @@ const TaskListNumbers = ({ data }) => {
   return (
     <div className="flex mt-10 justify-between screen gap-5">
       <div className="bg-red-400 w-[45%] rounded-xl py-6 px-9">
-        <h2 className="text-3xl font-semibold">0</h2>
+        <h2 className="text-3xl font-semibold">{data.taskStats.newTask}</h2>
         <h3 className="text-xl font-medium">New Task</h3>
       </div>
       <div className="bg-blue-400 w-[45%] rounded-xl py-6 px-9">
-        <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h2 className="text-3xl font-semibold">{data.taskStats.completed}</h2>
+        <h3 className="text-xl font-medium">Completed Task</h3>
       </div>
       <div className="bg-green-400 w-[45%] rounded-xl py-6 px-9">
-        <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h2 className="text-3xl font-semibold">{data.taskStats.active}</h2>
+        <h3 className="text-xl font-medium">Accepted Task</h3>
       </div>
       <div className="bg-yellow-400 w-[45%] rounded-xl py-6 px-9">
-        <h2 className="text-3xl font-semibold">0</h2>
-        <h3 className="text-xl font-medium">New Task</h3>
+        <h2 className="text-3xl font-semibold">{data.taskStats.failed}</h2>
+        <h3 className="text-xl font-medium">Failed Task</h3>
       </div>
     </div>
   );

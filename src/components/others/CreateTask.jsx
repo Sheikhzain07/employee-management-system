79595bb79690c51e166ba22578b8df2f34ec839a
Sheikhzain07 <sheikhzain459@gmail.com>
2024-11-12 00:20:07 +0530

    setting up the logic for creating task from admin dashboard

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateTask = () => {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+  const [taskAssignTo, setTaskAssignTo] = useState("");
+  const [category, setCategory] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(
+      "submitted task",
+      taskTitle,
+      taskDescription,
+      taskDate,
+      taskAssignTo,
+      category
+    );
+  };
   return (
     <div className="p-5 bg-[#1c1c1c] mt-5 rounded">
-      <form className="flex flex-wrap w-full items-start justify-between">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex flex-wrap w-full items-start justify-between"
+      >
         <div className="w-1/2">
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
             <input
+              value={taskTitle}
+              onChange={(e) => {
+                setTaskTitle(e.target.value);
+              }}
               className="w-4/5 py-1 px-2 text-sm rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="Make a UI design"
@@ -16,6 +42,10 @@ const CreateTask = () => {
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
             <input
+              value={taskDate}
+              onChange={(e) => {
+                setTaskDate(e.target.value);
+              }}
               className="w-4/5 py-1 px-2 text-sm rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="date"
             />
@@ -23,6 +53,10 @@ const CreateTask = () => {
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
             <input
+              value={taskAssignTo}
+              onChange={(e) => {
+                setTaskAssignTo(e.target.value);
+              }}
               className="w-4/5 py-1 px-2 text-sm rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="Employee name"
@@ -31,6 +65,10 @@ const CreateTask = () => {
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
             <input
+              value={category}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }}
               className="w-4/5 py-1 px-2 text-sm rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="design, dev etc"
@@ -40,6 +78,10 @@ const CreateTask = () => {
         <div className="w-2/5 flex flex-col items-start">
           <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
           <textarea
+            value={taskDescription}
+            onChange={(e) => {
+              setTaskDescription(e.target.value);
+            }}
             className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400"
             name=""
             id=""
